b1341cb7f8845ff1d5972e87e7e3913fe346e24b
Milosz08 <gilgamilosz451@gmail.com>
2022-10-11 14:50:33 +0200

    Initial commit

---
+++ util/src/main/java/io/jmpsl/util/Math.java
@@ -0,0 +1,11 @@
+package io.jmpsl.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Math {
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+}
