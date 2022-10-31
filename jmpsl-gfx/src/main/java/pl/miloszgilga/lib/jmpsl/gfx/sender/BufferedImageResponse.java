6fc3a2dfb79dbb7b1ead3ad6e77eb5f040a1892f
Milosz08 <gilgamilosz451@gmail.com>
2022-10-31 23:51:47 +0100

    create POJOs classes for GFX module

---
+++ jmpsl-gfx/src/main/java/pl/miloszgilga/lib/jmpsl/gfx/sender/BufferedImageResponse.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 by multiple authors
+ *
+ * File name: BufferedImageResponse.java
+ * Last modified: 31/10/2022, 23:30
+ * Project name: jmps-library
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.lib.jmpsl.gfx.sender;
+
+import lombok.*;
+
+import pl.miloszgilga.lib.jmpsl.file.BufferedFileResponse;
+
+/**
+ * Simple POJO class extending {@link BufferedFileResponse} class with additional <code>userHashCode</code> property
+ * (for user images).
+ */
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BufferedImageResponse extends BufferedFileResponse {
+    private String userHashCode;
+
+    /**
+     * Method available copy entire {@link BufferedImageResponse} instance into another {@link BufferedImageResponse}
+     * instance.
+     *
+     * @param payload instance of {@link BufferedImageResponse} POJO class
+     * @author Miłosz Gilga
+     * @since 1.0.2
+     */
+    public void copyObject(BufferedImageResponse payload) {
+        setBytesRepresentation(payload.getBytesRepresentation());
+        setLocation(payload.getLocation());
+        this.userHashCode = payload.userHashCode;
+    }
+}
---
+++ jmpsl-gfx/src/main/java/pl/miloszgilga/lib/jmpsl/gfx/sender/ImageExistPayload.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 by multiple authors
+ *
+ * File name: ImageExistPayload.java
+ * Last modified: 31/10/2022, 19:12
+ * Project name: jmps-library
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.lib.jmpsl.gfx.sender;
+
+import lombok.*;
+
+/**
+ * Simple POJO class stored result of method in {@link UserImageSftpSender} which available for checking if image already
+ * exist.
+ *
+ * <ul>
+ *     <li><code>dirPath</code> - base directory of image path</li>
+ *     <li><code>exist</code> - boolean flag, true if exist otherwise false</li>
+ * </ul>
+ *
+ * @author Miłosz Gilga
+ * @since 1.0.2
+ */
+@Data
+@AllArgsConstructor
+public class ImageExistPayload {
+    private String dirPath;
+    private boolean exist;
+}
