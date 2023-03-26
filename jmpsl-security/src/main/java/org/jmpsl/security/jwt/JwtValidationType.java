64bb36c681fb50686fbfb4e4ed47a91c014ff240
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-03-26 14:47:37 +0200

    update MIT copyright header content

--- jmpsl-security/src/main/java/org/jmpsl/security/jwt/JwtValidationType.java
+++ jmpsl-security/src/main/java/org/jmpsl/security/jwt/JwtValidationType.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 by multiple authors
  *
  * File name: JwtValidationType.java
- * Last modified: 14/02/2023, 20:59
+ * Last modified: 14/02/2023, 23:33
  * Project name: jmps-library
  *
  * Licensed under the MIT license; you may not use this file except in compliance with the License.
@@ -12,8 +12,14 @@
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
- * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL COPIES OR
+ * SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ *
+ * The software is provided “as is”, without warranty of any kind, express or implied, including but not limited
+ * to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event
+ * shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an
+ * action of contract, tort or otherwise, arising from, out of or in connection with the software or the use
+ * or other dealings in the software.
  */
 
 package org.jmpsl.security.jwt;
